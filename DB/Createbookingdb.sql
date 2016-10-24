4faa8a607843aa65c494ba8e4abdd72e8eb5cc42
jintaifeng <286340261@qq.com>
2016-10-24 15:24:27 +0800

    签入数据库

--- DB/Createbookingdb.sql
+++ DB/Createbookingdb.sql
@@ -1,5 +1,7 @@
-﻿create database GolfBooking 
+﻿-- 1. 데이타베이스 
+create database GolfBooking 
 
+--2. 테이블 
 use [GolfBooking]
 CREATE TABLE [dbo].[user] (
     [user_id]             UNIQUEIDENTIFIER DEFAULT NEWID() NOT NULL,
@@ -44,27 +46,31 @@ CREATE TABLE rbac_role_object (
 CREATE TABLE [dbo].[golf_course] (
   [course_id]     int identity(1,1) NOT NULL,
   [course_name]   NVARCHAR (50) NOT NULL,
+  [commission]   INT NOT NULL,
+  [status]     VARCHAR(10) NOT NULL,
   [description]   NVARCHAR (200) NULL,
   PRIMARY KEY(course_id)
 );
-CREATE TABLE [dbo].[order] (
+CREATE TABLE [dbo].[booking_order] (
   [order_id]  int identity(100,1) NOT NULL,
   [user_id]  UNIQUEIDENTIFIER NOT NULL,
   [course_id]  int NOT NULL,
-  [booking_name] nvarchar(50) not null,
-  [phone]   varchar(50) not null,
-  [order_status]      VARCHAR (20) NOT NULL,	
+  [booking_username] nvarchar(50) not null,
+  [phone]   varchar(50) not null,	
   [bookling_status]       varchar(20) not null,
   [settle_status]       varchar(20) not null,
   [member_number]  int not null,
-  [appointment_time]   DATETIME      NOT NULL,  
+  [appointment_date]   DATETIME      NOT NULL,  
+  [booking_date]   DATETIME      NOT NULL,  
   [deposit] int null,
   [pay_balance] int null,
+  [sub_total] int not null,
   [total] int not null,
-  [tax] int null,
-  [tstamp]             DATETIME      NOT NULL,
+  [commission] int null,
+  [total_commission] int null,
   [description]    nvarchar(200)  null,
-  [agency] nvarchar(50) null,
+  [tstamp]             DATETIME      NOT NULL,
+  [update_time]        DATETIME      NOT NULL,
   PRIMARY KEY(order_id)
 );
 CREATE TABLE [dbo].[user_code] (
@@ -72,7 +78,41 @@ CREATE TABLE [dbo].[user_code] (
 	[code] varchar(50) not null,
 	[tstamp]   DATETIME      NOT NULL
 );
+
+-- 3. 데이타 입력
+INSERT INTO [GolfBooking].[dbo].[rbac_roles]
+           ([role_name]
+           ,[description]
+           ,[tstamp])
+     VALUES
+           ('Admin','관리자',GETDATE()),
+           ('Custom','일반사용자',GETDATE())
 GO
+
+
+--4. admin insert
+
+INSERT INTO [dbo].[user]
+			(
+            [login_name]
+           ,[email]
+           ,[password]
+           ,[name]
+           ,[role_id]
+           ,[phone]
+           ,[status]
+           ,[update_time]
+           ,[tstamp])
+     VALUES
+           ('admin'
+           ,'admin@outlook.com'
+           ,'21232F297A57A5A743894A0E4A801FC3'
+           ,'admin'
+           ,1
+           ,''
+           ,'open'
+           ,GETDATE()
+           ,GETDATE())
 
 
 
